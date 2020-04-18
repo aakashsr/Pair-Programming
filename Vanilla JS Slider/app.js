d31848a47f2a99a4e1a58a484cedcc2dc785c1f0
aakashsr <aakashsri.dev@gmail.com>
2020-04-18 18:59:15 +0530

    added slideLeft , slideRight function to slide left/right and added these functions as event listeners on buttons

--- Vanilla JS Slider/app.js
+++ Vanilla JS Slider/app.js
@@ -1,6 +1,10 @@
 var lists = document.querySelectorAll("ul li");
+var current = 0;
 
 console.log(lists);
+
+hide(lists);
+displayFirst(lists);
 
 function hide(lists) {
   lists.forEach(function (cur) {
@@ -8,9 +12,37 @@ function hide(lists) {
   });
 }
 
-function startSlide() {
-  hide(lists);
+function displayFirst(lists) {
   lists[0].style.display = "block";
 }
 
-startSlide();
+function displaySlide(current) {
+  hide(lists);
+  lists[current].style.display = "block";
+}
+
+function slideLeft() {
+  hide(lists);
+  if (current === 0) {
+    current = lists.length;
+    current = current - 1;
+    displaySlide(current);
+  } else {
+    current = current - 1;
+    displaySlide(current);
+  }
+}
+
+function slideRight() {
+  hide(lists);
+  if (current === lists.length - 1) {
+    current = 0;
+    displaySlide(current);
+  } else {
+    current = current + 1;
+    displaySlide(current);
+  }
+}
+
+document.querySelector(".btn-left").addEventListener("click", slideLeft);
+document.querySelector(".btn-right").addEventListener("click", slideRight);
