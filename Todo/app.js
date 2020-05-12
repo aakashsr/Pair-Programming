08ddd22880b448599cbbbaefcb565ce6133a9f5e
aakashsr <aakashsri.dev@gmail.com>
2020-05-12 13:20:56 +0530

    stored the elements in a variable and refactored the code

--- Todo/app.js
+++ Todo/app.js
@@ -3,52 +3,51 @@
 // 3. create addListItem function
 // 4. Get data from fields
 // 5. clear the fields
-// 6. add event listener to delete the btn
+// 6. add event listener to delete btn
 // 7. create function to delete the item
 // 8. create function to clear tasks.
 // 9. create function to filter tasks.
+
+const newTask = document.querySelector("#task");
+const list = document.querySelector(".collection");
+const filterTask = document.querySelector("#filter");
+const clearTaskBtn = document.querySelector(".clear-tasks");
+const addTaskBtn = document.querySelector("button");
 
 var UIController = (function () {
   return {
     inputData: function () {
       return {
-        inputValue: document.querySelector("#task").value,
+        inputValue: newTask.value,
       };
     },
 
     displayInput: function (value) {
-      var html =
-        '<li class="collection-item">%description%<a href="#" class="delete-item secondary-content"><i class="fa fa-remove"></i></a></li>';
+      // creating placeholder
+      var html = `<li class="collection-item">%description%<a href="#" class="delete-item secondary-content"><i class="fa fa-remove"></i></a></li>`;
 
+      // replacing placeholder with actual data
       var newHTML = html.replace("%description%", value);
 
-      document
-        .querySelector(".collection")
-        .insertAdjacentHTML("beforeend", newHTML);
+      // inserting the element into the DOM.
+      list.insertAdjacentHTML("beforeend", newHTML);
+    },
+    clearInput() {
+      // 3. clear input field and refocus
+      newTask.value = "";
+      newTask.focus();
     },
   };
 })();
 
 var controller = (function (UICtrl) {
   // Add event listeners
+  addTaskBtn.addEventListener("click", (e) => ctrlAddListItem(e));
+  list.addEventListener("click", (e) => ctrlDeleteListItem(e));
+  clearTaskBtn.addEventListener("click", (e) => clearListItems(e));
+  filterTask.addEventListener("input", (e) => ctrlFilterItems(e));
 
-  document
-    .querySelector(".btn")
-    .addEventListener("click", (e) => ctrlAddListItem(e));
-
-  document
-    .querySelector(".collection")
-    .addEventListener("click", (e) => ctrlDeleteListItem(e));
-
-  document
-    .querySelector(".clear-tasks")
-    .addEventListener("click", (e) => clearListItems(e));
-
-  document
-    .getElementById("filter")
-    .addEventListener("input", (e) => ctrlFilterItems(e));
-
-  // unction  add and display item
+  // function  add and display item
   var ctrlAddListItem = function (e) {
     var data;
     e.preventDefault();
@@ -61,15 +60,11 @@ var controller = (function (UICtrl) {
       UICtrl.displayInput(data.inputValue);
     }
 
-    // 3. clear input field and refocus
-    document.getElementById("task").value = "";
-    document.getElementById("task").focus();
+    UICtrl.clearInput();
   };
 
   // function to delete item
   var ctrlDeleteListItem = function (e) {
-    var listItems = document.querySelectorAll(".collection-item");
-    var listItemsArray = Array.prototype.slice.call(listItems);
     var parent = e.target.parentNode.parentNode;
     const mainParent = parent.parentNode;
     if (parent.className === "collection-item") {
@@ -80,20 +75,18 @@ var controller = (function (UICtrl) {
   // function to clear all items
   var clearListItems = function (e) {
     e.preventDefault();
-    var listParent = document.querySelector(".collection");
-    var listItems = document.querySelectorAll(".collection-item");
-    var listItemsArray = Array.prototype.slice.call(listItems);
+    var listParent = list;
+    var listItemsArray = document.querySelectorAll(".collection-item");
     listItemsArray.forEach(function (cur) {
       listParent.removeChild(cur);
     });
-    document.getElementById("task").value = "";
-    document.getElementById("task").focus();
+    UICtrl.clearInput();
   };
 
   // function to filter items
   var ctrlFilterItems = function (e) {
-    var listItems = document.querySelectorAll(".collection-item");
-    var listItemsArray = Array.prototype.slice.call(listItems); // converting nodelists into array
+    var listItemsArray = document.querySelectorAll(".collection-item");
+    console.log(listItemsArray);
     listItemsArray.forEach(function (cur) {
       var content = cur.textContent.toLowerCase();
       if (content.includes(e.target.value)) {
